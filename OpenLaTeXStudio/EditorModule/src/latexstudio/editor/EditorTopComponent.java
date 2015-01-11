7c9a290dc35785368a97a8062d851a80d9076a9f
Sebastian Brudzinski <seb.brudzinski@gmail.com>
2015-01-11 23:04:27 +0100

    Added simple code completion
    The list will appear automatically after 700 miliseconds, after you start typing,
    or can be called manually, pressing ctrl + space

--- OpenLaTeXStudio/EditorModule/src/latexstudio/editor/EditorTopComponent.java
+++ OpenLaTeXStudio/EditorModule/src/latexstudio/editor/EditorTopComponent.java
@@ -1,9 +1,20 @@
 package latexstudio.editor;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import latexstudio.editor.util.ApplicationUtils;
+import org.apache.pdfbox.io.IOUtils;
+import org.fife.ui.autocomplete.AutoCompletion;
+import org.fife.ui.autocomplete.BasicCompletion;
+import org.fife.ui.autocomplete.CompletionProvider;
+import org.fife.ui.autocomplete.DefaultCompletionProvider;
 import org.netbeans.api.settings.ConvertAsProperties;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
+import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
 import org.openide.windows.TopComponent;
 
@@ -34,6 +45,7 @@ import org.openide.windows.TopComponent;
 public final class EditorTopComponent extends TopComponent {
     
     private boolean dirty = false;
+    private static final int AUTO_COMPLETE_DELAY = 700;
 
     public EditorTopComponent() {
         initComponents();
@@ -95,6 +107,13 @@ public final class EditorTopComponent extends TopComponent {
     @Override
     public void componentOpened() {
         ApplicationUtils.deleteTempFiles();
+        CompletionProvider provider = createCompletionProvider();
+
+        AutoCompletion ac = new AutoCompletion(provider);
+        ac.setAutoActivationDelay(AUTO_COMPLETE_DELAY);
+        ac.setAutoActivationEnabled(true);
+        ac.setAutoCompleteEnabled(true);
+        ac.install(rSyntaxTextArea);
     }
 
     @Override
@@ -128,4 +147,34 @@ public final class EditorTopComponent extends TopComponent {
         String version = p.getProperty("version");
         // TODO read your settings according to their version
     }
+    
+    private CompletionProvider createCompletionProvider() {
+        DefaultCompletionProvider provider = new DefaultCompletionProvider();
+        provider.setAutoActivationRules(true, "");
+
+        URL[] urls = new URL[3];
+        urls[0] = getClass().getResource("/latexstudio/editor/resources/tex.cwl");
+        urls[1] = getClass().getResource("/latexstudio/editor/resources/latex-document.cwl");
+        urls[2] = getClass().getResource("/latexstudio/editor/resources/latex-mathsymbols.cwl");
+
+        for (URL url : urls) {
+            InputStream is = null;
+            try {
+                is = url.openStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    if (!line.startsWith("#")) {
+                        provider.addCompletion(new BasicCompletion(provider, line.substring(1)));
+                    }
+                }
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            } finally {
+                IOUtils.closeQuietly(is);
+            }
+        }
+        
+        return provider;
+   }
 }
