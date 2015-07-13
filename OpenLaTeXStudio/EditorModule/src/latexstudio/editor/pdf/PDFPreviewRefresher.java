2fa6de07be111b6794326650cae1729a5f319a41
Labelpm <ayatsynyuk@provectus-it.com>
2015-07-13 16:08:38 +0200

    Fixed pdf preview undo rendo functionality

--- OpenLaTeXStudio/EditorModule/src/latexstudio/editor/pdf/PDFPreviewRefresher.java
+++ OpenLaTeXStudio/EditorModule/src/latexstudio/editor/pdf/PDFPreviewRefresher.java
@@ -41,7 +41,7 @@ public class PDFPreviewRefresher implements Runnable {
             if (etc.isDirty()) {
                 compileTemporaryFile();
                 drawPreview();
-                etc.setDirty(false);
+              //  etc.setDirty(false);
             }
             
             try {
@@ -52,7 +52,7 @@ public class PDFPreviewRefresher implements Runnable {
         }
     }
     
-    private void drawPreview() {
+    public void drawPreview() {
         JPanel pdfImagePanel = pdfDisplay.drawPreviewOnJPanel();
        
         if (pdfImagePanel != null) {
@@ -64,7 +64,7 @@ public class PDFPreviewRefresher implements Runnable {
         }
     }
 
-    private void compileTemporaryFile() {        
+    public void compileTemporaryFile() {        
         String fileLocation = ApplicationUtils.getTempSourceFile();
         String content = etc.getEditorContent();
         
