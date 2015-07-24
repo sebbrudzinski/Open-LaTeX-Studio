9decc43cbd0b792190cf765d8ed0247ced7cc3e0
Gabriel Felipe Diaz Cely <gabriel.diaz@seaq.co>
2015-07-24 16:27:39 -0500

    Add timestamps to logs issue #47
    Fixed loggerDateFormat access modifiers
    
    Fixing the line 106
    
    Fixing the line 106 that could cause some exceptions.
    
    Clear logs. Issue #48
    
    Adding the ability to clear logs. Only a button was added to clear the
    textArea.
    
    Check if pdflatex exists. Now in linux as well
    Verify the path pointed by the user as pdflatex (Tools -> options -> LaTeX) is valid. If the selected path does not contain pdflatex tool, the user is notified and asked to confirm the choice.
    
    Check if pdflatex exists. Now in linux as well
    Verify the path pointed by the user as pdflatex (Tools -> options -> LaTeX) is valid. If the selected path does not contain pdflatex tool, the user is notified and asked to confirm the choice.
    
    Implementing issue #39
    The code may look large, but I have made some fixies that had not been seen in #39
    Before this commit, if the user click GENERATE, and then CLICK on a pdf file, the Chooser would select "file.pdf". After that, pdflatex would generate the output onto "file.pdf.pdf", instead of overwriting the clicked file. So I fixed this. I remove the ".pdf" if the selected file has this name.
    
    Besides, I implemented the #39 issue. If the output file already exists, the program notify the user and asks to overwrite the file.
    
    Implementing issue #39
    Replacing "selected file" with the full path of the file to be overwrited.
    
    Update README.md
    
    Removed planned releases section

--- OpenLaTeXStudio/EditorModule/src/latexstudio/editor/ApplicationLogger.java
+++ OpenLaTeXStudio/EditorModule/src/latexstudio/editor/ApplicationLogger.java
@@ -5,6 +5,9 @@
  */
 package latexstudio.editor;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -17,13 +20,16 @@ public class ApplicationLogger {
     private final String loggerName;
     private final OutputTopComponent otc = new TopComponentFactory<OutputTopComponent>()
             .getTopComponent(OutputTopComponent.class.getSimpleName());
-
+    private final DateFormat loggerDateFormat = new SimpleDateFormat("MMM dd HH:mm:ss");
+    
+    
     public ApplicationLogger(String loggerName) {
         this.loggerName = loggerName;
     }
     
     public void log(String message) {
-        otc.logToOutput("[" + loggerName + "] " + message);
+		Date currentDate = new Date();
+        otc.logToOutput(loggerDateFormat.format(currentDate) + " [" + loggerName + "] " + message);
     }
     
     public void log(String[] messages) {
