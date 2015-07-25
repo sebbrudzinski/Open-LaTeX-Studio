3a59b20146ef25b4eac91e7ea2fd39c17c732c53
Hudson Geovane <hudsongeovane@gmail.com>
2015-07-24 22:38:36 -0300

    Solving the issue #46 Verify the path pointed by the user as pdflatex (Tools -> options -> LaTeX) is valid. If the selected path does not contain pdflatex tool, the user is notified and asked to confirm the choice.
    There was a bug in line 103
    
    We have to check if the directory is null before to verify the existence of pdflatex.exe.
    
    Fixing the line 106
    
    Fixing the line 106 that could cause some exceptions.
    
    Clear logs. Issue #48
    
    Adding the ability to clear logs. Only a button was added to clear the
    textArea.
    
    Check if pdflatex exists. Now in linux as well
    Verify the path pointed by the user as pdflatex (Tools -> options -> LaTeX) is valid. If the selected path does not contain pdflatex tool, the user is notified and asked to confirm the choice.
    
    Check if pdflatex exists. Now in linux as well
    Verify the path pointed by the user as pdflatex (Tools -> options -> LaTeX) is valid. If the selected path does not contain pdflatex tool, the user is notified and asked to confirm the choice.

--- OpenLaTeXStudio/EditorModule/src/latexstudio/editor/OutputTopComponent.java
+++ OpenLaTeXStudio/EditorModule/src/latexstudio/editor/OutputTopComponent.java
@@ -54,11 +54,19 @@ public final class OutputTopComponent extends TopComponent {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
 
         jTextArea1.setEditable(false);
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
+
+        org.openide.awt.Mnemonics.setLocalizedText(jButton1, org.openide.util.NbBundle.getMessage(OutputTopComponent.class, "OutputTopComponent.jButton1.text")); // NOI18N
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -66,19 +74,30 @@ public final class OutputTopComponent extends TopComponent {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jButton1)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 278, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        jTextArea1.setText("");
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
