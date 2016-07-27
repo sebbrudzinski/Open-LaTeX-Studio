2103e0404880648fcce66c2815f93def7970b6ba
Stefan Suciu <stefbv70@gmail.com>
2016-07-27 08:55:35 +0300

    Fix imports

--- OpenLaTeXStudio/Editor/src/main/java/latexstudio/editor/util/ApplicationUtils.java
+++ OpenLaTeXStudio/Editor/src/main/java/latexstudio/editor/util/ApplicationUtils.java
@@ -5,14 +5,8 @@
  */
 package latexstudio.editor.util;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.regex.Pattern;
-import org.apache.commons.io.IOUtils;
 import org.openide.util.Exceptions;
 
 /**
